9231ce77f8611f61ee63c183ae2e48e9b183dd67
northandhisfriends-arch <northandhisfriends@gmail.com>
2025-11-05 22:24:44 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // **เพิ่มบรรทัดนี้เพื่อแก้ปัญหา Base Path**
+
   base: "/ai-detect/",
 
   server: {
